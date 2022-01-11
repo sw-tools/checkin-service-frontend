0fe8d3bf45868137564eb2976b700a47561d48f9
SW Tools <swtools0@gmail.com>
2022-01-10 20:59:31 -0700

    Formatting with prettier, readme updates npm updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify'
+import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: netlify(),
+  kit: {
+    adapter: netlify(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte'
+  }
 };
 
 export default config;
