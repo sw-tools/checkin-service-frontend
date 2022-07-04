16804880087f6c46139acecf1b7ae442dfc246db
SW Tools <swtools0@gmail.com>
2022-07-04 11:40:40 -0600

    Update create checkin flow so that it works

--- src/lib/components/create-checkin-form.svelte
+++ src/lib/components/create-checkin-form.svelte
@@ -1,43 +1,32 @@
 <script lang="ts">
   import HttpStatus from 'http-status';
-  import * as Luxon from 'luxon';
+  import type { PostRequestBody } from '../../routes/checkins/new.json';
 
-  let confirmation_number = '';
-  let first_name = '';
-  let last_name = '';
+  let confirmationNumber = '';
+  let firstName = '';
+  let lastName = '';
   let showMessage = false;
   let responseMessage = '';
 
   async function submitForm() {
-    const response = await fetch('/checkins/index.json', {
+    const requestBody: PostRequestBody = {
+      data: {
+        confirmation_number: confirmationNumber,
+        first_name: firstName,
+        last_name: lastName
+      }
+    };
+    const response = await fetch('/checkins/new.json', {
       method: 'POST',
-      body: JSON.stringify({ data: { confirmation_number, first_name, last_name } })
+      body: JSON.stringify(requestBody)
     });
 
     if (response.status === HttpStatus.OK) {
-      const responseBody: ResponseBody = await response.json();
-
-      const checkinAvailable = Luxon.DateTime.fromSeconds(
-        responseBody.data.checkin_available_epoch
-      ).toLocaleString(Luxon.DateTime.DATETIME_FULL_WITH_SECONDS);
-
-      showMessage = true;
-      responseMessage = `Checkin scheduled successfully. You'll be checked in ${checkinAvailable}.`;
-      return;
-    }
-
-    if ([HttpStatus.BAD_REQUEST, HttpStatus.UNPROCESSABLE_ENTITY].includes(response.status)) {
+      window.location.href = '/checkins';
+    } else if ([HttpStatus.BAD_REQUEST, HttpStatus.UNPROCESSABLE_ENTITY].includes(response.status)) {
       showMessage = true;
       responseMessage = 'Unable to schedule checkin';
-      return;
     }
-  }
-
-  interface ResponseBody {
-    data: {
-      checkin_available_epoch: number;
-      checkin_boot_epoch: number;
-    };
   }
 </script>
 
@@ -47,47 +36,92 @@
       <h3 class="font-extrabold text-3xl">{responseMessage}</h3>
     </div>
   {:else}
-    <div class="text-center">
-      <h3 class="font-extrabold text-3xl">Schedule a checkin</h3>
-      <form class="" on:submit|preventDefault={submitForm}>
-        <label for="email" class="label">
-          <span class="sr-only">Confirmation number</span>
-        </label>
-        <input
-          id="confirmation_number"
-          aria-label="confirmation number"
-          type="text"
-          name="confirmation_number"
-          autocomplete="off"
-          required
-          bind:value={confirmation_number}
-        />
-        <label for="email" class="label">
-          <span class="sr-only">First name</span>
-        </label>
-        <input
-          id="first_name"
-          aria-label="first name"
-          type="text"
-          name="first_name"
-          autocomplete="on"
-          required
-          bind:value={first_name}
-        />
-        <label for="email" class="label">
-          <span class="sr-only">Last name</span>
-        </label>
-        <input
-          id="last_name"
-          aria-label="last name"
-          type="text"
-          name="last_name"
-          autocomplete="on"
-          required
-          bind:value={last_name}
-        />
-        <input type="submit" />
-      </form>
+    <div class="flex items-center justify-center px-4 sm:px-6 lg:px-8">
+      <div class="max-w-md w-full space-y-8">
+        <div>
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Schedule a checkin</h2>
+        </div>
+
+        <div class="rounded bg-white max-w-md overflow-hidden shadow-xl p-5">
+          <form class="space-y-4" on:submit|preventDefault={submitForm}>
+            <div class="rounded-md shadow-sm -space-y-px">
+              <div class="grid">
+                <div class="col-span-12">
+                  <label for="confirmationNumber" class="label pb-1">
+                    <span class="label-text">Confirmation Number</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="confirmationNumber"
+                    id="confirmationNumber"
+                    autocomplete="off"
+                    class="input input-bordered input-secondary w-full"
+                    aria-label="confirmation number"
+                    required
+                    bind:value={confirmationNumber}
+                  />
+                </div>
+
+                <div class="col-span-12">
+                  <label for="confirmationNumber" class="label pb-1">
+                    <span class="label-text">First Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    autocomplete="given-name"
+                    class="input input-bordered input-secondary w-full"
+                    aria-label="first name"
+                    required
+                    bind:value={firstName}
+                  />
+                </div>
+
+                <div class="col-span-12">
+                  <label for="confirmationNumber" class="label pb-1">
+                    <span class="label-text">Last Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                    autocomplete="family-name"
+                    class="input input-bordered input-secondary w-full"
+                    aria-label="last name"
+                    required
+                    bind:value={lastName}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div class="flex items-center justify-between">
+              <div class="flex items-center">
+                <input
+                  id="remember_me"
+                  name="remember_me"
+                  type="checkbox"
+                  class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label for="remember_me" class="ml-2 block text-sm text-gray-900">
+                  Watch this flight for a better price
+                </label>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span class="absolute left-0 inset-y-0 flex items-center pl-3">🚀</span>
+                Schedule
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   {/if}
 </div>
---
+++ src/routes/checkins/new.json.ts
@@ -0,0 +1,95 @@
+import type { EndpointOutput, RequestHandler } from '@sveltejs/kit';
+import type { MaybePromise } from '@sveltejs/kit/types/helper';
+import type { ServerRequest } from '@sveltejs/kit/types/hooks';
+import HttpStatus from 'http-status';
+
+export type PostRequestBody = {
+  data: {
+    confirmation_number: string;
+    first_name: string;
+    last_name: string;
+  };
+};
+
+export type PostResponseBody = {
+  data: {
+    added_checkin_times: {
+      checkin_available_epoch: number;
+      checkin_boot_epoch: number;
+    }[];
+    already_scheduled_checkin_times: {
+      checkin_available_epoch: number;
+      checkin_boot_epoch: number;
+    }[];
+  };
+};
+
+export type PostErrorResponseBody = {
+  error: string;
+};
+
+/**
+ * This pass-through keeps endpoint and authorizer token secret from the frontend
+ */
+export const post: RequestHandler<never, string, PostResponseBody | PostErrorResponseBody> = async (
+  req
+) => {
+  let responseBody: MaybePromise<void | EndpointOutput<PostResponseBody | PostErrorResponseBody>>;
+  try {
+    responseBody = await postInternal(req);
+  } catch (error) {
+    console.error(error);
+    return {
+      status: HttpStatus.INTERNAL_SERVER_ERROR,
+      // hide the external API's error from the public with a generic error message
+      body: { error: 'Internal server error' }
+    };
+  }
+  return responseBody;
+};
+
+async function postInternal(req: ServerRequest<never, string>) {
+  const requestBody = JSON.parse(req.body);
+  const externalRequestBody: ExternalRequestBody = {
+    data: {
+      confirmation_number: requestBody.data.confirmation_number,
+      first_name: requestBody.data.first_name,
+      last_name: requestBody.data.last_name
+    }
+  };
+  const res = await fetch(`${process.env.HOST_URL}/v1/checkin-service/checkin`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      token: process.env.AUTHORIZER_TOKEN
+    },
+    body: JSON.stringify(externalRequestBody)
+  });
+
+  const responseBody = (await res.json()) as ExternalResponseBody;
+  return {
+    status: res.status,
+    body: responseBody
+  };
+}
+
+interface ExternalRequestBody {
+  data: {
+    confirmation_number: string;
+    first_name: string;
+    last_name: string;
+  };
+}
+
+interface ExternalResponseBody {
+  data: {
+    added_checkin_times: {
+      checkin_available_epoch: number;
+      checkin_boot_epoch: number;
+    }[];
+    already_scheduled_checkin_times: {
+      checkin_available_epoch: number;
+      checkin_boot_epoch: number;
+    }[];
+  };
+}
