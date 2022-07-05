9e737e8377aa5c45c2d9bd5d8f3e8dbde5d4bfad
SW Tools <swtools0@gmail.com>
2022-07-04 23:11:56 -0600

    Minor improvements

--- src/routes/checkins/index.svelte
+++ src/routes/checkins/index.svelte
@@ -7,19 +7,15 @@
   <div class="mb-10">
     <div class="flex items-center justify-center px-4 sm:px-6 lg:px-8">
       <div class="max-w-md w-full space-y-8">
-        <div>
-          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Scheduled Checkins</h2>
-        </div>
-
         {#if $user}
+          <div>
+            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Scheduled Checkins</h2>
+          </div>
           <div class="flex justify-end">
             <a href="/checkins/new" class="button hover:no-underline">New</a>
           </div>
           {#each $checkins as checkin (`${checkin.confirmation_number}${checkin.first_name}${checkin.last_name}`)}
             <div class=" card w-full bg-base-100 shadow-xl overflow-visible">
-              <!-- <div class=" -left-5 -top-5 z-99">
-                <Fa size="3x" color="gray" icon={faPlaneUp} />
-              </div> -->
               <div class="card-body">
                 <div class="card-actions">
                   <div class="badge badge-success text-white">Scheduled</div>
