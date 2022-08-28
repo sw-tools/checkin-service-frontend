7ef889165a7b872228c53df907d4d0d93a172da9
SW Tools <swtools0@gmail.com>
2022-08-28 06:18:36 -0600

    Test function locally, fix bug

--- netlify/functions/caffeinate-planetscale.ts
+++ netlify/functions/caffeinate-planetscale.ts
@@ -5,7 +5,7 @@ import { prisma } from "../../src/server/db/client";
  * Serverless cron job that keeps free tier Planetscale database alive
  */
 const handler: Handler = async () => {
-  prisma.caffeinateMessage.create({ data: { message: "Stay awake!" } });
+  await prisma.caffeinateMessage.create({ data: { message: "Stay awake!" } });
 
   return { statusCode: 200 };
 };
